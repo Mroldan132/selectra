4a212a4639ac953976e5b46f51bb903ffee771d5
Kathia <kathiabg@gmail.com>
2025-06-19 00:34:21 -0500

    creación y edición de áreas

---
+++ Selectra/DTOs/DetalleAreaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Selectra.DTOs
+{
+    public class DetalleAreaDto
+    {
+        public int areaId { get; set; }
+        public string nombreArea { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public DateTime? fechaUltMod { get; set; }
+
+    }
+}
--- Selectra/Services/Areas/AreaService.cs
+++ Selectra/Services/Areas/AreaService.cs
@@ -20,5 +20,51 @@ namespace Selectra.Services.Areas
                     Descripcion = a.descripcion,
                 })
                 .ToListAsync();
+        public async Task<bool> GenerarAreaAsync(DetalleAreaDto areaDto)
+        {
+            var area = new Area
+            {
+                nombreArea = areaDto.nombreArea,
+                descripcion = areaDto.descripcion,
+                fechaCreacion = DateTime.Now,
+                fechaUltMod = DateTime.Now
+            };
+            _context.Areas.Add(area);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> ActualizarAreaAsync(int idArea, ActualizarAreaDto actualizarAreaDto)
+        {
+            if (idArea <= 0)
+            {
+                throw new ArgumentException("El ID del area es necesario para insertar el requerimiento.", nameof(idArea));
+            }
+
+            var area = await _context.Areas.FindAsync(idArea);
+
+            if (area == null)
+            {
+                throw new ApplicationException($"No se encontró el area con ID {idArea}.");
+            }
+
+            area.nombreArea = actualizarAreaDto.NombreArea;
+            area.descripcion = actualizarAreaDto.Descripcion;
+            area.fechaUltMod = DateTime.UtcNow;
+
+            using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                _context.Areas.Update(area);
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return true;              
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
     }
 }
